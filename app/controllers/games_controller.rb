5cf261f5eb5768d285c45195ba035d8da607e647
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-04-10 22:15:47 -0400

    mongoid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,4 +19,15 @@ class GamesController < ApplicationController
     Game.where(_id: params[:_id]).update(currentScore: params["game"]["currentScore"])
     redirect_to game_path
   end
+
+  # def get_connection
+  #   return @db_connection if @db_connection
+  #   db = URI.parse(ENV['MONGOHQ_URL'])
+  #   db_name = db.path.gsub(/^\//, '')
+  #   @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  #   @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  #   @db_connection
+  # end
+ 
+  # db = get_connection
 end
