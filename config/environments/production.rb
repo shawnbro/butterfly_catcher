5e4f22c9659a4783ade1fe1ec819d6a3b6df2f9a
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-04-18 15:18:00 -0700

    jkl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ ButterflyCatcher::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( search.js )
+  # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
 
   # Ignore bad email addresses and do not raise email delivery errors.
